5da76b957667e840d56b0d329ef2042a7a8dba96
Igor <igorcherstnev@gmail.com>
2023-05-09 00:52:17 +0100

    Cenas

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/script.js" defer></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Ticket Manager</title>
+</head>
+<body> 
+    <?php include_once("templates/sidebar.php");?>  
+    <div class = "main">
+        <h1>Ticket Manager</h1>
+    </div>
+</body>
+</html>
---
+++ templates/sidebar.php
@@ -0,0 +1,26 @@
+<div class="sidebar">
+        <a href="#">
+            <span class="icon"><i class="fa fa-qrcode"></i></span>
+            <span class="text">Home</span>
+        </a>
+        <br>
+        <a href="#">
+            <span class="icon"><i class="fa fa-link"></i></span>
+            <span class="text">Account</span>
+        </a>
+        <br>
+        <a href="#">
+            <span class="icon"><i class="fa fa-eye"></i></span>
+            <span class="text">Tickets</span>
+        </a>
+        <br>
+        <a href="#">
+            <span class="icon"><i class="fa fa-book"></i></span>
+            <span class="text">Settings</span>
+        </a>
+        <br>
+        <a href="#">
+            <span class="icon"><i class="fa fa-question-circle"></i></span>
+            <span class="text">About</span>
+        </a>
+</div>
