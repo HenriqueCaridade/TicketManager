017f0845b5e4f1f1eaffdb3db96ecb65d9334460
Henrique Caridade <up202108817@up.pt>
2023-05-09 01:06:30 +0100

    Cenas

--- database/connection.php
+++ database/connection.php
@@ -2,19 +2,20 @@
     function getDatabaseConnection(){
         return new PDO('sqlite:tables.db');
     }
+
     function getUser(string $username, PDO $db){
         $stmt = $db->prepare('SELECT * FROM User WHERE username = :username');
         $stmt->bindParam(':username', $username);
         $stmt->execute();
         return $stmt->fetch();
     }
-    /*
+/*
   $stmt = $db->prepare('SELECT * FROM User');
   $stmt->execute();
   $users = $stmt->fetchAll();
-  foreach( $users as $property) {
-    echo '<h1>' . $property['username'] . '</h1>';
-    echo '<p>' . $property['name'] . '</p>';
-  };*/
-
+  foreach ($users as $user) {
+    echo '<h1>' . $user['username'] . '</h1>';
+    echo '<p>' . $user['name'] . '</p>';
+  };
+*/
 ?>
