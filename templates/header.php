8637561c4e2a158c8800d96ee90152d5d647ead0
Henrique Caridade <up202108817@up.pt>
2023-05-09 19:18:32 +0100

    Added basic CSS and separation between templates and pages.

---
+++ templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/script.js" defer></script>
+    <script src="https://kit.fontawesome.com/f3cf9d3f6c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Ticket Manager</title>
+</head>
+<body>
+    <header>
+        <span class="logo">Timan</span>
+    </header>
