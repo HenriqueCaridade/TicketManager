03d2e70a7fb6d3acdf216bb38f2b9021aed2a809
Henrique Caridade <up202108817@up.pt>
2023-05-16 00:45:13 +0100

    Added missing files.

---
+++ classes/faq.php
@@ -0,0 +1,39 @@
+<?php
+    class FAQ {
+        public int $id;
+        public string $question;
+        public string $answer;
+
+        private function __construct(int $id, string $question, string $answer) {
+            $this->id = $id;
+            $this->question = $question;
+            $this->answer = $answer;
+        }
+        private static function fromArray(array $comment) : FAQ {
+            return new FAQ((int) $comment['id'], $comment['question'], $comment['answer']);
+        }
+        public static function getFAQ(PDO $db, int $id) : ?FAQ {
+            $stmt = $db->prepare('SELECT * FROM FAQ WHERE id=?');
+            $stmt->execute(array($id));
+            $faq = $stmt->fetch();
+            if ($faq === false) return null;
+            return FAQ::fromArray($faq);
+        }
+
+        public static function createFAQ(PDO $db, string $question, string $answer) : void {
+            $stmt = $db->prepare('INSERT INTO FAQ (question, answer) VALUES (?, ?)');
+            $stmt->execute(array($question, $answer));
+        }
+
+        public static function getAll(PDO $db) : array {
+            $stmt = $db->prepare('SELECT * FROM FAQ');
+            $stmt->execute();
+            $faqs = $stmt->fetchAll();
+            $faqArray = array();
+            foreach ($faqs as $faq) {
+                $faqArray[] = FAQ::fromArray($faq);
+            }
+            return $faqArray;
+        }
+    }
+?>
---
+++ classes/ticketComment.php
@@ -0,0 +1,45 @@
+<?php
+    class TicketComment {
+        public int $id;
+        public int $ticketId;
+        public ?string $user; 
+        public DateTime $date;
+        public string $text;
+
+        private function __construct(int $id, int $ticketId, string $user, DateTime $date, string $text) {
+            $this->id = $id;
+            $this->ticketId = $ticketId;
+            $this->user = $user;
+            $this->date = $date;
+            $this->text = $text;
+        }
+        private static function fromArray(array $comment) : TicketComment {
+            return new TicketComment((int) $comment['id'], (int) $comment['ticketId'], $comment['user'], new DateTime($comment['date']), $comment['text']);
+        }
+        public static function getTicketComment(PDO $db, int $id) : ?TicketComment {
+            $stmt = $db->prepare('SELECT * FROM TicketComment WHERE id=?');
+            $stmt->execute(array($id));
+            $comment = $stmt->fetch();
+            if ($comment === false) return null;
+            return TicketComment::fromArray($comment);
+        }
+
+        public static function createTicketComment(PDO $db, int $ticketId, string $user, DateTime $date, string $text) : void {
+            $stmt = $db->prepare('INSERT INTO TicketComment (ticketId, user, date, text) VALUES (?, ?, ?, ?)');
+            $stmt->execute(array($ticketId, $user, $date->format('Y-m-d H:i:s'), $text));
+        }
+
+        public static function getTicketComments(PDO $db, int $id) : array {
+            $stmt = $db->prepare('SELECT * FROM TicketComment WHERE ticketId=?');
+            $stmt->execute(array($id));
+            $comments = $stmt->fetchAll();
+            $commentArray = array();
+            foreach ($comments as $comment) {
+                $commentArray[] = TicketComment::fromArray($comment);
+            }
+            // Order from newer to older
+            usort($commentArray, fn(TicketComment $a, TicketComment $b) => ($a->date > $b->date) ? 1 : (($a->date < $b->date) ? -1 : 0));
+            return $commentArray;
+        }
+    }
+?>
